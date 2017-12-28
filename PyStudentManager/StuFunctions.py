a7852c57d3ed4c5012f5a9f7bdfde3206d0dafef
SunDeep <p.sundeep@live.com>
2017-12-29 00:31:36 +0530

    On branch master  Your branch is ahead of 'origin/master' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   .idea/workspace.xml
    	modified:   PyStudentManager/StuFunctions.py
    	new file:   PyStudentManager/studentdetails.txt

--- PyStudentManager/StuFunctions.py
+++ PyStudentManager/StuFunctions.py
@@ -16,10 +16,27 @@ def addStudent(name,student_id=999):
         student = {"name":name,"StudentID":student_id}
         students.append(student)
 
-student_name = str(input("Enter Student Name:"))
-student_id = int(input("Enter Student ID and the Default ID is 999:"))
-addStudent(student_name, student_id)
+def save_studetails(student_name,student_id):
+    f = open('studentdetails.txt', 'a')
+    f.write(f'{student_name},{student_id}\n')
+    f.close()
+
+def read_studetails():
+    f = open('studentdetails.txt', 'r')
+    allstulist = f.readlines()
+    for student in allstulist:
+        student_temp = student.split(',', 1)
+        student_name = student_temp[0]
+        student_ID = student_temp[1]
+        print(f'StudentID:{student_ID}\tStudentName:{student_name}')
+
+read_studetails()
+
+#student_name = str(input("Enter Student Name:"))
+#student_id = int(input("Enter Student ID and the Default ID is 999:"))
+# addStudent(student_name, student_id)
+#save_studetails(student_name, student_id)
 
 
-print(students)
-#print_student_title_list()
+
+
